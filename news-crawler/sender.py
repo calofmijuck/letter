3109bd109711cabe50aaa7f0f00472f38a696e28
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-05-01 20:32:59 +0900

    feat: news crawler (#3)
    * feat: news cralwer and sender
    
    * feat: write Dockerfile
    
    * docs: Update docs

---
+++ news-crawler/sender.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+import thecampy
+
+from config import EMAIL, PASSWORD, NAME, BIRTH, ENTER_DATE, UNIT_NAME
+
+SOLDIER = thecampy.Soldier(
+    NAME, BIRTH, ENTER_DATE, UNIT_NAME
+)
+
+
+def send(subject, content):
+    content += '\n[FINISH]'
+    msg_num = 0
+    char_count, enter_count = 0, 0
+    buffer = []
+    for i, char in enumerate(content):
+        buffer.append(char)
+        char_count += 1
+        if char == '\n':
+            enter_count += 1
+
+        if char_count > 1495 or enter_count > 22 or i == len(content) - 1:
+            _send(subject + f" - {msg_num}", ''.join(buffer))
+
+            char_count, enter_count = 0, 0
+            buffer = []
+
+            msg_num += 1
+
+
+def _send(subject, content):
+    try:
+        message = thecampy.Message(subject, content)
+        client = thecampy.client()
+        client.login(EMAIL, PASSWORD)
+        client.get_soldier(SOLDIER)
+        client.send_message(SOLDIER, message)
+        print(f"[+] {datetime.now()} - NEWS SENT")
+    except:
+        print(f"[-] {datetime.now()} - SEND FAILED")
